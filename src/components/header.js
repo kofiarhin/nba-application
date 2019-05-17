fcec75d795a9eb25163652fab810a9d955ba4053
Kofi <info@kofiarhin.com>
2019-05-17 06:48:15 +0100

    testing stuff

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from "react";
 import style from "./header.css";
 
 const Header = () => {
-  return <header className={style.main_header}> Main headersss</header>;
+  return <header className={style.main_header}> Main header </header>;
 };
 
 export default Header;
