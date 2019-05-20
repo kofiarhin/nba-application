e0d8473242155d313d4e3f71443919842c7bf41e
Kofi <info@kofiarhin.com>
2019-05-20 01:02:09 +0100

    added header options

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,7 +1,31 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import style from "./header.css";
+import Fontawesome from "react-fontawesome";
 const Header = () => {
-  return <header> new header</header>;
+  const logo = () => {
+    return (
+      <Link to="/">
+        <img src="/images/nba_logo.png" />
+      </Link>
+    );
+  };
+
+  const bars = () => {
+    return (
+      <div className="bars">
+        <Fontawesome name="bars" />
+      </div>
+    );
+  };
+  return (
+    <header className={style.header}>
+      <div className={style.headerOpt}>
+        <div className={style.bars}>{bars()} </div>
+        <div>{logo()}</div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
