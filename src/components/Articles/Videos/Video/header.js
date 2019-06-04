78b487e90bc7f7a1626aa5f1feaf1ee356584b59
Kofi <info@kofiarhin.com>
2019-06-04 23:02:21 +0100

    file restructure

--- src/components/Articles/Videos/Video/header.js
+++ src/components/Articles/Videos/Video/header.js
@@ -11,34 +11,13 @@ const Header = (props) => {
         return team ? <TeamInfo team={team} /> : null;
     }
 
-    const renderVideo = () => {
 
-
-        const video = props.video;
-
-        return (
-
-            <div className={styles.videoWrapper}>
-
-                <h1> {video.title} </h1>
-
-                <iframe
-                    title="videoplayer"
-                    width="100%"
-                    height="300px"
-                    src={`http://www.youtube.com/embed/${video.ur}`}
-                > </iframe>
-
-
-            </div>
-        )
-    }
 
     return (
 
         <div>
             {renderTeamInfo(props.team)}
-            {renderVideo()}
+
         </div>
     )
 
--- src/components/Articles/Videos/Video/index.js
+++ src/components/Articles/Videos/Video/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 import { URL } from "../../../../config";
 
-
+import styles from "../../articles.css";
 //custom components
 import Header from "./header";
 
@@ -37,16 +37,42 @@ class VideoArticle extends Component {
 
     renderVideo = () => {
 
-        return <div> render video </div>
+        const video = this.state.video;
+
+        const title = video.title;
+        const url = video.url;
+
+        return (
+
+            <div className={styles.content}>
+                <h1>{title}</h1>
+                <iframe
+
+                    title="videoplayer"
+                    width="100%"
+                    height="300px"
+                    src={`http://www.youtube.com/embed/${url}`}
+                > </iframe>
+            </div>
+
+        )
     }
+
     render() {
 
         return (
 
-            <Header
-                video={this.state.video}
-                team={this.state.team[0]}
-            />
+            <div className={styles.videoWrapper}>
+                <Header
+                    video={this.state.video}
+                    team={this.state.team[0]}
+                />
+
+                {this.renderVideo()}
+            </div>
+
+
+
         )
     }
 }
